4b345a3ad208d5fb8bdedf0654c80d1099873ebe
luizinho <luizinhoo08.pereira@gmail.com>
2025-03-31 14:50:33 -0300

    while comando break

--- while/while01.py
+++ while/while01.py
@@ -1,4 +1,4 @@
-contador = 1
+contador = 5
 while contador <= 5:
     print(contador)
-    contador = contador + 1
+    contador = contador + 5
---
+++ while/while5.py
@@ -0,0 +1,6 @@
+numero = int(input("insira o numero"))
+while True: #loop infinito
+        numero = numero - 1
+        print(numero)
+        if numero == 0:
+            break
